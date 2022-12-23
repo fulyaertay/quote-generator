e64b8b9ea8b78e3ce0237de5319f48393ec2b8d6
Fulya <fulya.ertay@gmail.com>
2022-12-23 22:12:01 +0300

    feat:get quote

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import{useState} from 'react';
+import{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const[quotes, setQuotes]=useState();
+  //get quote
+  const getQuote=()=>{
+    fetch('https://type.fit/api/quotes')
+    .then((res)=>res.json())
+    .then((data)=>{
+      let randomNum=Math.floor(Math.random()*data.length)
+      setQuotes(data[randomNum])
+
+    });
+  }
+  
   return (
     <div className="App">
+      <button onClick={getQuote}>Get Quote</button>
     
     </div>
   );
