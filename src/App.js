dabb14e22f418d65b0e5082b470406c68b69fbee
Fulya <fulya.ertay@gmail.com>
2022-12-23 22:53:42 +0300

    feat: add box css

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const[quotes, setQuotes]=useState();
+  const[quotes, setQuotes]=useState("");
   //get quote
   const getQuote=()=>{
     fetch('https://type.fit/api/quotes')
     .then((res)=>res.json())
     .then((data)=>{
-      let randomNum=Math.floor(Math.random()*data.length)
-      setQuotes(data[randomNum])
+      let randomNum=Math.floor(Math.random()*data.length);
+      setQuotes(data[randomNum]);
 
     });
   }
   useEffect(()=>{
     getQuote();
-  })
+  },[])
   return (
     <div className="App">
-      <button onClick={getQuote}>Get Quote</button>
+     <div className='quote'>
+      <p>«{quotes.text}»</p>
+      <p>-{quotes.author}</p>
+      <div className='btnContainer'>
+        <button onClick={getQuote} className="btn">Get Quote</button>
+      </div>
+     </div>
     
     </div>
   );
